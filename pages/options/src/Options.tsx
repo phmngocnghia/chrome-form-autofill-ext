d63e4e5f4375a23f273ab220d594c8b9ec04aec8
Nghia Pham <phmngocnghia@gmail.com>
2025-01-10 10:19:41 +0700

    add option page

--- pages/options/src/Options.tsx
+++ pages/options/src/Options.tsx
@@ -1,28 +1,76 @@
+import React, { useState } from 'react';
 import '@src/Options.css';
-import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
-import { exampleThemeStorage } from '@extension/storage';
-import { Button } from '@extension/ui';
+import { withErrorBoundary, withSuspense } from '@extension/shared';
+import { Drawer, List, ListItem, ListItemButton, CssBaseline, Box } from '@mui/material';
+import { HashRouter, Routes, Route, NavLink } from 'react-router';
 
-const Options = () => {
-  const theme = useStorage(exampleThemeStorage);
-  const isLight = theme === 'light';
-  const logo = isLight ? 'options/logo_horizontal.svg' : 'options/logo_horizontal_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
+import { type GridRowsProp, type GridColDef, DataGrid } from '@mui/x-data-grid';
+
+const initialRows: GridRowsProp = [
+  { id: 1, fieldName: 'Hello', fieldExpr: 'World', fieldValue: 'Demo' },
+  { id: 2, fieldName: 'DataGridPro', fieldExpr: 'is Awesome' },
+  { id: 3, fieldName: 'MUI', fieldExpr: 'is Amazing' },
+];
+
+const initialColumns: GridColDef[] = [
+  { field: 'fieldName', headerName: 'Rule Name', editable: true, flex: 1 },
+  { field: 'fieldExpr', headerName: 'Field Expression', editable: true, flex: 1 },
+  { field: 'fieldValue', headerName: 'Field Value', editable: true, flex: 1 },
+];
+
+const drawerWidth = 150;
+
+function App() {
+  const [rows, setRows] = useState<GridRowsProp>(initialRows);
+
+  const handleDeleteRow = (id: number) => {
+    setRows(prevRows => prevRows.filter(row => row.id !== id));
+  };
+
+  const columns: GridColDef[] = [
+    ...initialColumns,
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      renderCell: params => <button onClick={() => handleDeleteRow(params.row.id)}>Delete</button>,
+    },
+  ];
 
   return (
-    <div className={`App ${isLight ? 'bg-slate-50 text-gray-900' : 'bg-gray-800 text-gray-100'}`}>
-      <button onClick={goGithubSite}>
-        <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-      </button>
-      <p>
-        Edit <code>pages/options/src/Options.tsx</code>
-      </p>
-      <Button className="mt-4" onClick={exampleThemeStorage.toggle} theme={theme}>
-        Toggle theme
-      </Button>
+    <div className="flex">
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        open
+        anchor="left">
+        <List>
+          {['Rules', 'Presents'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                {/* <ListItemText primary={text} /> */}
+                {/* <NavLink to={index === 0 ? '/' : '/rules'}>{text}</NavLink> */}
+                {text}
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box sx={{ p: 4, width: `calc(100% - ${drawerWidth}px)` }}>
+        <DataGrid rows={rows} columns={columns} />
+      </Box>
+      {/* <Routes>
+          <Route path='/' element={<DataGrid rows={rows} columns={columns} />} />
+          <Route path='/rules' element={<DataGrid rows={rows} columns={columns} />} />
+        </Routes> */}
     </div>
   );
-};
-
-export default withErrorBoundary(withSuspense(Options, <div> Loading ... </div>), <div> Error Occur </div>);
+}
+export default withErrorBoundary(withSuspense(App, <div> Loading ... </div>), <div> Error Occur </div>);
